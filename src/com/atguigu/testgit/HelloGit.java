a121ed1d05017822c8137eedb6a6c2e9c0deba7c
shybbq <757823054@qq.com>
2016-07-09 08:42:55 +0800

    22
    22

--- src/com/atguigu/testgit/HelloGit.java
+++ src/com/atguigu/testgit/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 public static void main(String[] args) {
 	System.out.println("update 1");
 	System.out.println("Servlet update");
+	System.out.println("Servlet update1");
 }
 }
